239149b10679ac3af5f789d837485c047a700766
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-08-11 08:57:07 +0200

    creation des pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { TechniquesComponent } from './pages/techniques/techniques.component';
+import { ProfilComponent } from './pages/profil/profil.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ConnexionComponent } from './pages/connexion/connexion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PageHomeComponent,
+    TechniquesComponent,
+    ProfilComponent,
+    InscriptionComponent,
+    NotFoundComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
