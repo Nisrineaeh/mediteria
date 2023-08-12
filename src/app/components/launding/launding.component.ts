84cbad8f27be3e51d68aa8833e789455208b8c92
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-08-12 23:00:33 +0200

    recup donnee api + affichage liste sur launding page ok

--- src/app/components/launding/launding.component.ts
+++ src/app/components/launding/launding.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TechniqueMeditation } from 'src/app/models/technique-meditation';
+import { TechniquesService } from 'src/app/services/techniques.service';
 
 @Component({
   selector: 'app-launding',
   templateUrl: './launding.component.html',
   styleUrls: ['./launding.component.css']
 })
-export class LaundingComponent {
+export class LaundingComponent implements OnInit{
 
-}
+  techniques: TechniqueMeditation[]= [];
+  constructor(private techService: TechniquesService){}
+
+  ngOnInit(): void {
+    this.loadTech();
+  }
+
+  loadTech(){
+    this.techService.geteTechnique().subscribe({
+      next: (data) => {
+        this.techniques = data;
+      },
+      error: (err) => {
+        console.error('Erreur lors de la recup des données', err);
+      }
+    })
+    }
+  }
+
+
