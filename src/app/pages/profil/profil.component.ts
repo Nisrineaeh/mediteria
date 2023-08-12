c290b02706d58cef1a93ba620c0efb9f1182816c
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-08-13 00:08:03 +0200

    page profil service ok + affichage

--- src/app/pages/profil/profil.component.ts
+++ src/app/pages/profil/profil.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TechniqueMeditation } from 'src/app/models/technique-meditation';
+import { TechSelectService } from 'src/app/services/tech-select.service';
 
 @Component({
   selector: 'app-profil',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./profil.component.css']
 })
 export class ProfilComponent {
+  monUnivers: TechniqueMeditation[]=[];
+
+  constructor(private techService: TechSelectService){
+    this.monUnivers = this.techService.getSelectedTech();
+  }
 
 }
