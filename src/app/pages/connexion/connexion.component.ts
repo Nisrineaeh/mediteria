bce49869573dc2257bfb0b328d971b8274de8323
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-08-14 14:16:36 +0200

    service auth ok

--- src/app/pages/connexion/connexion.component.ts
+++ src/app/pages/connexion/connexion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 // import { AuthServiceService } from 'src/app/services/auth-service.service';
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,5 +6,11 @@ import {tap} from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:8080/utilisateur/api/login';
 
+  constructor(private http: HttpClient) { }
+
+  login(username: string, mot_de_passe: string) {
+    return this.http.post(this.apiUrl, { username, mot_de_passe });
+  }
 }
