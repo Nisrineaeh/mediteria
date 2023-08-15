b92d6f6b8b0b00128f3d8abf88cc555447eade22
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-08-15 11:18:30 +0200

    essaye recup donnee bdd

--- src/app/pages/profil/profil.component.ts
+++ src/app/pages/profil/profil.component.ts
@@ -12,7 +12,7 @@ import { UtilisateursService } from 'src/app/services/utilisateurs.service';
 })
 export class ProfilComponent implements OnInit{
   monUnivers: TechniqueMeditation[]=[];
-  userData: any = {};
+  userData!: Utilisateur;
  
 
   constructor(private techService: TechSelectService, private userService: UtilisateursService, private router: Router){
@@ -21,9 +21,20 @@ export class ProfilComponent implements OnInit{
   ngOnInit(): void {
     const userEmail = localStorage.getItem('user_email');
     if (userEmail) {
-      this.userService.findByEmail(userEmail).subscribe(data => {
-        this.userData = data;
-        console.log(this.userData);
+      this.userService.findByEmail(userEmail).subscribe({
+        next: (data: Utilisateur | null) => {
+          if (data) { // Vérifiez si les données ne sont pas nulles
+            this.userData = data;
+            console.log(this.userData);
+          } else {
+            console.error('Aucune donnée utilisateur trouvée pour cet email.');
+          }
+        },
+        error: error => {
+          console.error('Erreur lors de la récupération des données utilisateur:', error);
+        }
+        // Si vous voulez également gérer la complétion, ajoutez:
+        // complete: () => { console.log('Complété'); }
       });
     } else {
       console.error('Email non trouvé dans le stockage local');
